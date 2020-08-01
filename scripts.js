add11db0328c62d5f3e4675f0855c936594c5294
chihchiu26 <chih.chiu@network.rca.ac.uk>
2020-08-02 02:33:17 +0800

    initial setup

---
+++ scripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $('.burgermenu').on('click', function () {
+        $('.projectList').toggle();
+    })
+
+});
+
+
+document.getElementsByClassName('img_hover1')[0].onmouseover = function () {
+    document.getElementsByClassName('nav_highlight1')[0].style = "color: #444444 ; text-decoration:underline"
+}
+document.getElementsByClassName('img_hover1')[0].onmouseout = function () {
+    document.getElementsByClassName('nav_highlight1')[0].style = ""
+}
+
+
+document.getElementsByClassName('img_hover2')[0].onmouseover = function () {
+    document.getElementsByClassName('nav_highlight2')[0].style = "color: #444444 ; text-decoration:underline"
+}
+document.getElementsByClassName('img_hover2')[0].onmouseout = function () {
+    document.getElementsByClassName('nav_highlight2')[0].style = ""
+}
+
+
+document.getElementsByClassName('img_hover3')[0].onmouseover = function () {
+    document.getElementsByClassName('nav_highlight3')[0].style = "color: #444444 ; text-decoration:underline"
+}
+document.getElementsByClassName('img_hover3')[0].onmouseout = function () {
+    document.getElementsByClassName('nav_highlight3')[0].style = ""
+}
+
+
+document.getElementsByClassName('img_hover4')[0].onmouseover = function () {
+    document.getElementsByClassName('nav_highlight4')[0].style = "color: #444444 ; text-decoration:underline"
+}
+document.getElementsByClassName('img_hover4')[0].onmouseout = function () {
+    document.getElementsByClassName('nav_highlight4')[0].style = ""
+}
+
+
+document.getElementsByClassName('img_hover5')[0].onmouseover = function () {
+    document.getElementsByClassName('nav_highlight5')[0].style = "color: #444444 ; text-decoration:underline"
+}
+document.getElementsByClassName('img_hover5')[0].onmouseout = function () {
+    document.getElementsByClassName('nav_highlight5')[0].style = ""
+}
+
+
+document.getElementsByClassName('img_hover6')[0].onmouseover = function () {
+    document.getElementsByClassName('nav_highlight6')[0].style = "color: #444444 ; text-decoration:underline"
+}
+document.getElementsByClassName('img_hover6')[0].onmouseout = function () {
+    document.getElementsByClassName('nav_highlight6')[0].style = ""
+}
+
