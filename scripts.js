683ee096a001936a508d44263289e9d57f05e906
chihchiu26 <chih.chiu@network.rca.ac.uk>
2020-08-09 23:29:52 +0800

    add about

--- scripts.js
+++ scripts.js
@@ -1,87 +1,16 @@
 $(document).ready(function () {
     $('.burgermenu').on('click', function () {
         $('.projectList').slideToggle('fast');
+    });
+
+    var texts = document.querySelectorAll('.projectItems a');
+    [].slice.call(document.querySelectorAll('.thumbnailGrid a')).forEach(function (a, i){
+        a.onmouseover = function () {
+            texts[i].style = "color: #333333 ; text-decoration:underline"
+        }
+        a.onmouseout = function () {
+            texts[i].style = ""
+        }
     })
 
 });
-
-
-document.getElementsByClassName('img_hover1')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight1')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover1')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight1')[0].style = ""
-}
-
-
-document.getElementsByClassName('img_hover2')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight2')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover2')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight2')[0].style = ""
-}
-
-
-document.getElementsByClassName('img_hover3')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight3')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover3')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight3')[0].style = ""
-}
-
-
-document.getElementsByClassName('img_hover4')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight4')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover4')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight4')[0].style = ""
-}
-
-
-document.getElementsByClassName('img_hover5')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight5')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover5')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight5')[0].style = ""
-}
-
-
-document.getElementsByClassName('img_hover6')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight6')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover6')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight6')[0].style = ""
-}
-
-
-
-document.getElementsByClassName('img_hover7')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight7')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover7')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight7')[0].style = ""
-}
-
-
-document.getElementsByClassName('img_hover8')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight8')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover8')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight8')[0].style = ""
-}
-
-
-document.getElementsByClassName('img_hover9')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight9')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover9')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight9')[0].style = ""
-}
-
-
-document.getElementsByClassName('img_hover10')[0].onmouseover = function () {
-    document.getElementsByClassName('nav_highlight10')[0].style = "color: #333333 ; text-decoration:underline"
-}
-document.getElementsByClassName('img_hover10')[0].onmouseout = function () {
-    document.getElementsByClassName('nav_highlight10')[0].style = ""
-}
