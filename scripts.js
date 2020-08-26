a74aeb6f0b10afedfab735009059be80bfc66614
chihchiu26 <chih.chiu@network.rca.ac.uk>
2020-08-27 02:17:59 +0800

    test

--- scripts.js
+++ scripts.js
@@ -20,12 +20,12 @@ var x = window.matchMedia("(max-width: 992px)")
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (x.matches){
+  // if (x.matches){
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-50px";
   }
-}
+// }
   prevScrollpos = currentScrollPos;
 }
