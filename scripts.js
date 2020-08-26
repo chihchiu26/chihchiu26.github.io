c48a4a915495c334d9572cb38ac543f943158bf6
chihchiu26 <chih.chiu@network.rca.ac.uk>
2020-08-27 02:14:16 +0800

    js

--- scripts.js
+++ scripts.js
@@ -14,3 +14,18 @@ $(document).ready(function () {
     })
 
 });
+
+
+var x = window.matchMedia("(max-width: 992px)")
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (x.matches){
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+  prevScrollpos = currentScrollPos;
+}
